68bff4a6a4dcb949e2228f7a99ae218976301b3d
raphpay <41484808+raphpay@users.noreply.github.com>
2022-10-10 09:27:36 +0200

    Feature: Initial commit:

---
+++ MinimalistBudget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MinimalistBudget
+//
+//  Created by Raphaël Payet on 10/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MinimalistBudget/MinimalistBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalistBudgetApp.swift
+//  MinimalistBudget
+//
+//  Created by Raphaël Payet on 10/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MinimalistBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
