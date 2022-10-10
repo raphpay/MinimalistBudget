ab2d553d7ee965de1c43129d32153b9887856734
raphpay <41484808+raphpay@users.noreply.github.com>
2022-10-10 11:32:23 +0200

    Feature: Create UI of the amount screen #1

--- MinimalistBudget/View/Screens/AmountView.swift
+++ MinimalistBudget/View/Screens/AmountView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct AmountView: View {
+    
+    @State private var category = "Besoins"
+    @State private var amount = ""
+    let categories = ["Besoins", "Désirs", "Envies"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Picker("What is your favorite color?", selection: $category) {
+                ForEach(categories, id: \.self) {
+                    Text($0)
+                }
+            }
+            .pickerStyle(.segmented)
+            
+            TextField("Montant", text: $amount)
+                .keyboardType(.decimalPad)
+                .textFieldStyle(.roundedBorder)
+            
+            Button {
+                //
+            } label: {
+                Text("Entrer la dépense")
+            }
+
+            
+            Spacer()
+        }
+        .padding()
     }
 }
 
